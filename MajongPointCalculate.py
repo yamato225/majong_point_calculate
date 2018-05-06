faf73cfbb4bc6f2d8ee4139535ac401bd5c21ebd
kazutaka <yamato0024@hotmail.com>
2018-05-07 00:31:25 +0900

    ４メンツ１雀頭のパターン識別処理を追加。

--- MajongPointCalculate.py
+++ MajongPointCalculate.py
@@ -2,23 +2,152 @@
 # -*- coding: utf-8 -*-
 
 TILES_NUM_INHANDS=14
+TILES_NUM_MAX=18        #四槓子の場合
+
+class MajongRoleDetection:
+    def __init__(self,handtiles,agari,furotiles=[],tenpai_check=False):
+        # todo:テンパイチェック機能をつける。
+        # todo:カンチェック機能をつける。
+        # ポン・チーの場合の条件分岐をつける。
+        # カンの場合どうするか・・・
+        if len(handtiles) + len(furotiles) > TILES_NUM_MAX - 1:
+            raise(ValueError ("too many tiles"))
+        self.handtiles = sorted(handtiles)
+        self.furotiles = sorted(furotiles)
+        self.agari = agari
+
+    def getTiles(self):
+        return {'tiles': self.handtiles, 'agari':self.agari} 
+
+    def isKokushiMusou(self,juusanmen=False):
+        kokushi_hands = [1,9,11,19,21,29,31,32,33,34,41,42,43]
+        alltiles = self.handtiles[:]        #copy list
+        if juusanmen == True:
+            return set(alltiles) == set(kokushi_hands) and self.agari in kokushi_hands
+        alltiles.append(self.agari)     #add tsumo to list
+        return set(alltiles) == set([1,9,11,19,21,29,31,32,33,34,41,42,43])
+
+    def isChitoitsu(self):
+        alltiles = self.handtiles[:]
+        alltiles.append(self.agari)
+        for tile in set(alltiles):
+            if alltiles.count(tile) != 2:
+                return False
+
+        return True
+
+    def append_pattern(self,pattern_list,pattern):
+        flag=True
+        for p in pattern_list:
+            if p == pattern:
+                flag=False
+        if flag == True:
+            p2 = {
+                "atama": pattern["atama"],
+                "anko": pattern["anko"],
+                "shuntsu": pattern["shuntsu"],
+                "remain": pattern["remain"]
+            }
+            pattern_list.append(p2)
+
+    def mentsu_pattern(self,patterns,results):
+        next_patterns = []
+        for p in patterns:
+            shuntsu = list(p["shuntsu"])
+            anko =  list(p["anko"])
+            shuntsutiles = list(p["remain"])
+            for t in shuntsutiles:
+                if shuntsutiles.count(t) > 0 and shuntsutiles.count(t+1) > 0 and shuntsutiles.count(t+2) > 0:
+                    shuntsu.append(t)
+                    for i in [0,1,2]:
+                        shuntsutiles.remove(t+i)
+                    p2 = {
+                        "atama":int(p["atama"]),
+                        "anko":sorted(anko),
+                        "shuntsu":sorted(shuntsu),
+                        "remain":sorted(shuntsutiles)
+                        }
+                    self.append_pattern(next_patterns,p2)
+                    break
+
+            shuntsu = list(p["shuntsu"])
+            anko =  list(p["anko"])
+            ankotiles = list(p["remain"])
+            for t in ankotiles:
+                num = ankotiles.count(t)
+                if num == 3:
+                    anko.append(t)
+                    for i in [0,1,2]:       #３個消す
+                        ankotiles.remove(t)
+                    p2 = {
+                        "atama":int(p["atama"]),
+                        "anko":sorted(anko),
+                        "shuntsu":sorted(shuntsu),
+                        "remain":sorted(ankotiles)
+                        }
+                    self.append_pattern(next_patterns,p2)
+                    break
+
+        #print(next_patterns)
+        #print "\n"
+        #print "results="+str(results)
+
+        flag=True
+        for np in next_patterns:
+            if len(np["remain"]) < 1:
+                r={"atama":int(np["atama"]),"anko":sorted(list(np["anko"])),"shuntsu":sorted(list(np["shuntsu"]))}
+                results.append(r)
+            else:
+                flag=False
+
+        if flag==False:
+            self.mentsu_pattern(next_patterns,results)
+                
+    def agari_pattern(self):
+        alltiles = self.handtiles[:]
+        alltiles.append(self.agari)
+        #２枚以上あるのを頭候補としてとする
+        patterns = []
+        results = []
+        for tile in set(alltiles):
+            num = alltiles.count(tile)
+            if num in [2,3]:
+                remain_tile=alltiles[:]
+                for i in [0,1]:       #２個消す
+                    remain_tile.remove(tile)
+                p = {"remain":remain_tile[:], "atama": tile, "anko":[], "shuntsu":[]}
+                patterns.append(p)
+
+        results = []
+        self.mentsu_pattern(patterns,results)
+
+        print "r="+str(results)
+        return results
+
+    def removeAnko(self,remain_tiles):
+        pass
+
+
+    def isToitoi(self):
+        pass
+
+    def isSuanko(self):
+        pass
+
+    def isDaisangen(self):
+
+        pass
+    
 
 class MajongPointCalculate:
     tiles = []
 
     def __init__(self,tiles,tsumo):
-        if len(tiles) != TILES_NUM_INHANDS - 1:
-            raise ValueError("wrong tiles")
         self.tiles = sorted(tiles)
         self.tsumo = tsumo
 
     def getTiles(self):
         return {'tiles': self.tiles, 'tsumo':self.tsumo} 
-
-    def isKokushiMusou(self):
-        alltiles = self.tiles[:]        #copy list
-        alltiles.append(self.tsumo)     #add tsumo to list
-        return set(alltiles) == set([1,9,11,19,21,29,31,32,33,34,41,42,43])
 
 if __name__=='__main__':
     pass
--- test_MajongPointCalculate.py
+++ test_MajongPointCalculate.py
@@ -1,40 +1,115 @@
+
+# -*- coding: utf-8 -*-
+
 import unittest
 from MajongPointCalculate import MajongPointCalculate
+from MajongPointCalculate import MajongRoleDetection
 
 class TestMajongPointCalculate(unittest.TestCase):
-    def test_setvaluecheck_errorcase(self):
-        mytile = [1,2,3,4]
+    #def test_setvaluecheck_errorcase(self):
+    #ポン・カン・チーの場合増減あるからチェックしない。
+    def test_setvalue_check_errorcase(self):
+        mytile = [1,2,3,4,1,2,3,4,5,6,7,8,9,11,12,13,13,14,14]
         mytsumo = 2
         with self.assertRaises(ValueError):
-            MajongPointCalculate(mytile,mytsumo)
+            MajongRoleDetection(mytile,mytsumo)
 
     def test_setvaluecheck_normal(self):
         mytile = [1,2,3,4,5,6,7,8,9,11,12,13,14]
         mytsumo = 2
-        mycalc = MajongPointCalculate(mytile,mytsumo)
+        mycalc = MajongRoleDetection(mytile,mytsumo)
         self.assertEqual(mycalc.getTiles(),{
             'tiles':[1,2,3,4,5,6,7,8,9,11,12,13,14],
-            'tsumo': 2
+            'agari': 2
+            })
+
+    def test_setvaluecheck_normal2(self):
+        mytile = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5]    #四槓子
+        mytsumo = 5
+        mycalc = MajongRoleDetection(mytile,mytsumo)
+        self.assertEqual(mycalc.getTiles(),{
+            'tiles':[1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5],
+            'agari': 5
             })
     
     def test_isKokushi1(self):
         mytile = [31,31,33,34,41,42,43,1,9,11,19,21,29]
         mytsumo = 32
-        mycalc = MajongPointCalculate(mytile,mytsumo)
+        mycalc = MajongRoleDetection(mytile,mytsumo)
         self.assertEqual(mycalc.isKokushiMusou(),True)
 
     def test_isKokushi2(self):
         mytile = [31,32,33,34,41,42,43,1,9,11,19,22,29]
         mytsumo = 31
-        mycalc = MajongPointCalculate(mytile,mytsumo)
+        mycalc = MajongRoleDetection(mytile,mytsumo)
         self.assertEqual(mycalc.isKokushiMusou(),False)
 
     def test_isKokushi3(self):
         mytile = [31,31,33,34,41,42,43,1,9,11,19,21,29]
         mytsumo = 32
-        mycalc = MajongPointCalculate(mytile,mytsumo)
+        mycalc = MajongRoleDetection(mytile,mytsumo)
         self.assertEqual(mycalc.isKokushiMusou(),True)
+
+    def test_isKokushi4(self):
+        mytile = [31,32,33,34,41,42,43,1,9,11,19,21,29]
+        mytsumo = 31
+        mycalc = MajongRoleDetection(mytile,mytsumo)
+        self.assertEqual(mycalc.isKokushiMusou(juusanmen=True),True)
+
+    def test_isKokushi5(self):
+        mytile = [32,32,33,34,41,42,43,1,9,11,19,21,29]
+        mytsumo = 31
+        mycalc = MajongRoleDetection(mytile,mytsumo)
+        self.assertEqual(mycalc.isKokushiMusou(juusanmen=True),False)
+
+    def test_isChitoitsu(self):
+        mytile = [1,1,12,12,13,13,4,4,5,5,6,6,7]
+        myagari = 7
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.isChitoitsu(),True)
+
+    def test_isChitoitsu2(self):
+        mytile = [1,1,12,12,13,13,4,4,5,5,7,7,7]
+        myagari = 7
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.isChitoitsu(),False)
+
+    def test_agari_pattern(self):
+        mytile = [1,1,1,2,2,2,11,11,11,21,21,21,24]
+        myagari = 24
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.agari_pattern(),[{'shuntsu': [], 'anko': [1, 2, 11, 21], 'atama': 24}])
+
+    def test_agari_pattern2(self):
+        mytile = [1,2,3,2,2,2,11,11,11,21,21,21,24]
+        myagari = 24
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.agari_pattern(),[{'anko': [2,11, 21], 'atama': 24, 'shuntsu': [1]}])
+
+    def test_agari_pattern3(self):
+        mytile = [2,3,4,2,3,4,2,3,4,6,6,6,8]    #四暗刻
+        myagari = 8
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.agari_pattern(),[{'shuntsu': [2, 2, 2], 'anko': [6],'atama': 8},{'shuntsu': [], 'anko': [2, 3, 4, 6], 'atama': 8}])  
+
+    def test_agari_pattern4(self):
+        mytile = [2,2,3,4,13,14,15,31,31,31,7,8,9]
+        myagari = 2
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.agari_pattern(),[{'anko': [31], 'atama': 2, 'shuntsu': [2,7,13]}]) 
+
+    def test_agari_pattern5(self):
+        mytile = [2,2,2,2,3,4,11,11,11,11,12,13,9]
+        myagari = 9
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.agari_pattern(),[{'anko': [2,11], 'atama': 9, 'shuntsu': [2,11]}])
     
+    def test_agari_pattern6(self):
+        mytile = [2,3,4,3,4,5,4,5,6,5,6,7,18]
+        myagari = 18
+        mycalc = MajongRoleDetection(mytile,myagari)
+        self.assertEqual(mycalc.agari_pattern(),[{'anko': [], 'atama': 18, 'shuntsu': [2,3,4,5]}])  
+
 
 if __name__ == '__main__':
     unittest.main()
